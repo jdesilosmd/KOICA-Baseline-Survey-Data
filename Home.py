86cef8381a0224b1c63230a9cc50846741c8d3bc
Jeriel De Silos <jdesilosmd@gmail.com>
2024-11-04 20:03:08 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,20 +10,20 @@ url = "https://onedrive.live.com/download?resid=8452D606ADDC0A0F!142073&ithint=f
 df = pd.read_excel(url)
 
 # Data assignment
-'''
-df2 = df.iloc[:, [3, 8, 9, ]+list(range(8, 26))]
-df3 = df.iloc[:, [3]+list(range(26, 74))]
-df4 = df.iloc[:, [3]+list(range(74, 111))]
-df5 = df.iloc[:, [3]+list(range(111, 144))]
-df6 = df.iloc[:, [3]+list(range(144, 154))]
-df7 = df.iloc[:, [3]+list(range(154, 196))]
-df8 = df.iloc[:, [3]+list(range(196, 430))]
-df9 = df.iloc[:, [3]+list(range(430, 460))]
-df10 = df.iloc[:, [3]+list(range(460, 601))]
-df11 = df.iloc[:, [3]+list(range(601, 645))]
-df12 = df.iloc[:, [3]+list(range(645, 746))]
 
-st.write(df)
-'''
+# df2 = df.iloc[:, [3, 8, 9, ]+list(range(8, 26))]
+# df3 = df.iloc[:, [3]+list(range(26, 74))]
+# df4 = df.iloc[:, [3]+list(range(74, 111))]
+# df5 = df.iloc[:, [3]+list(range(111, 144))]
+# df6 = df.iloc[:, [3]+list(range(144, 154))]
+# df7 = df.iloc[:, [3]+list(range(154, 196))]
+# df8 = df.iloc[:, [3]+list(range(196, 430))]
+# df9 = df.iloc[:, [3]+list(range(430, 460))]
+# df10 = df.iloc[:, [3]+list(range(460, 601))]
+# df11 = df.iloc[:, [3]+list(range(601, 645))]
+# df12 = df.iloc[:, [3]+list(range(645, 746))]
+
+# st.write(df)
+
 
 st.write('Under Construction')
